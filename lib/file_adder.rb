5b0f3dc621c3426f0bb0199a555fbc014df7d459
Mike Porter <mike@agileedge.com>
2017-04-03 15:03:01 -0700

    Implmented file summing assuming each row is an int

--- lib/file_adder.rb
+++ lib/file_adder.rb
@@ -1,8 +1,6 @@
 class FileAdder
   def initialize(filename)
-    @file = File.open(filename)
-    @sum = @file.inject(0) {|m, v| m += v.to_i }
-    @file.close
+    @sum = File.open(filename) {|f| f.inject(0) {|m, v| m += v.to_i }}
   end
 
   def sum
